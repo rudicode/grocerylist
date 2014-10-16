bec5b55873d5bf37015189dce28b4e419fe2c946
Rudi Luzar <rudi@rudicode.com>
2014-10-16 09:49:46 -0400

    Added the Items controller and some item views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,44 @@
 class ItemsController < ApplicationController
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
 	def index
 		@items = Item.all
 	end
+
+  # GET /items/1
+	def show
+		
+	end
+
+  # GET /items/new
+	def new
+		@item = Item.new
+	end
+
+  # POST /items
+	def create
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to items_url, notice: 'Item Succesfully saved'
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to items_url, notice: 'Item deleted'
+	end
+
+
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+	  def item_params
+	  	params.require(:item).permit(:name)
+	  end
 end
